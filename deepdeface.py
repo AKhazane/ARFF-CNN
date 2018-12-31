460ed202519a063d699829b586799f9b99824a4b
Anish Khazane <akkhazan@berkeley.edu>
2018-12-30 23:13:29 -0800

    add dice coef

--- deepdeface.py
+++ deepdeface.py
@@ -10,8 +10,15 @@ import time
 
 from keras import backend as K
 from keras.models import *
-from metrics import dice_coefficient
 
+
+''' Dice Coefficient Metric '''
+def dice_coefficient(y_true, y_pred, smooth=1.):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 def pre_process_image(img_file):
 
